1119d79face7ccbc94df452bf50a5c9040227cda
Yevhen Beskubskyi <yevhen.beskubskyi@gmail.com>
2021-06-20 17:59:54 +0300

    Create 0001.py

---
+++ 0001.py
@@ -0,0 +1,13 @@
+def two_sum(nums, target):
+    '''
+    Leetcode 0001 (Easy)
+    Two Sum
+    '''
+    diff = dict()
+    
+    for i, num in enumerate(nums):
+        if num in diff:
+            return [diff[num], i]
+        diff[target-num] = i
+    
+    return [-1, -1]

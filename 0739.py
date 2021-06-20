c1af26629458f171a03066997c36e44490b4d8af
Yevhen Beskubskyi <yevhen.beskubskyi@gmail.com>
2021-06-20 15:57:35 +0300

    Create 0739.py

---
+++ 0739.py
@@ -0,0 +1,15 @@
+def daily_temperatures(temperatures):
+    '''
+    Leetcode 0739 (Medium)
+    Daily Temperatures
+    '''
+    stack = []
+    res = [0] * len(temperatures)
+    
+    for i, t in enumerate(temperatures):
+        while stack and stack[-1][0] < t:
+            tmp, idx = stack.pop()
+            res[idx] = i - idx
+        stack.append([t, i])
+    
+    return res

6a00af60f0626a57f624ac7dc247c5454e08d332
Yevhen Beskubskyi <yevhen.beskubskyi@gmail.com>
2021-06-20 15:44:54 +0300

    Update 0022.py

--- 0022.py
+++ 0022.py
@@ -1,26 +1,26 @@
 def generate_parentheses(n):
-	'''
-	Leetcode 0022 (Medium)
-	Generate Parentheses
-	'''
-	parentheses = []
-	stack = []
+    '''
+    Leetcode 0022 (Medium)
+    Generate Parentheses
+    '''
+    parentheses = []
+    stack = []
 
-	def backtrack(o, c):
-		if o == c == n:
-			s = ''.join(stack)
-			parentheses.append(s)
-			return
+    def backtrack(o, c):
+        if o == c == n:
+            s = ''.join(stack)
+            parentheses.append(s)
+            return
 
-		if o < n:
-			stack.append('(')
-			backtrack(o+1, c)
-			stack.pop()
+        if o < n:
+            stack.append('(')
+            backtrack(o+1, c)
+            stack.pop()
 
-		if c < o:
-			stack.append(')')
-			backtrack(o, c+1)
-			stack.pop()
+        if c < o:
+            stack.append(')')
+            backtrack(o, c+1)
+            stack.pop()
 
-	backtrack(0, 0)
-	return parentheses
+    backtrack(0, 0)
+    return parentheses

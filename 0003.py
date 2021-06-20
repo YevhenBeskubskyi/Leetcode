fd50f9c9f3d450a8cb658d87177dbdf457a524f8
Yevhen Beskubskyi <yevhen.beskubskyi@gmail.com>
2021-06-20 15:40:31 +0300

    Update 0003.py

--- 0003.py
+++ 0003.py
@@ -1,18 +1,18 @@
 def length_of_longest_substring(s):
-	'''
-	Leetcode 0003
-	Difficulty: Medium
-	Longest Substring Without Repeating Character
-	'''
-	letters = set()
-	max_len = 0
+    '''
+    Leetcode 0003
+    Difficulty: Medium
+    Longest Substring Without Repeating Character
+    '''
+    letters = set()
+    max_len = 0
 
     j = 0
-	for i in range(len(s)):
-		while s[i] in letters:
-			letters.remove(s[j])
-			j += 1
-		letters.add(s[i])
-		max_len = max(max_len, len(letters))
+    for i in range(len(s)):
+        while s[i] in letters:
+            letters.remove(s[j])
+            j += 1
+        letters.add(s[i])
+        max_len = max(max_len, len(letters))
 
-	reurn max(max_len, len(letters))
+    reurn max(max_len, len(letters))

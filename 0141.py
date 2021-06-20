4bdb0f9c7e1e3f6d51972fcadc5e6b4d9a4c7cec
Yevhen Beskubskyi <yevhen.beskubskyi@gmail.com>
2021-06-20 16:23:15 +0300

    Create 0141.py

---
+++ 0141.py
@@ -0,0 +1,21 @@
+class ListNode:
+    
+    def __init__(self, val):
+        self.val = val
+        self.next = None
+
+def has_cycle(head):
+    '''
+    Leetcode 0141 (Easy)
+    Linked List Cycle
+    '''
+    fast = head
+    slow = head
+    
+    while fast and fast.next:
+        fast = fast.next.next
+        slow = slow.next
+        if fast == slow:
+            return True
+    
+    return False

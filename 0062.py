1833c631e40f0757ef69bbc060733d128635b086
Yevhen Beskubskyi <yevhen.beskubskyi@gmail.com>
2021-06-22 09:22:41 +0300

    Update 0062.py

--- 0062.py
+++ 0062.py
@@ -1,7 +1,6 @@
 def unique_paths(m, n):
     '''
-    Leetcode 0062 (Medium)
-    Unique Paths
+    Leetcode 0062 (Medium): Unique Paths
     '''
     grid = [[1] * n for _ in range(m)]
     for i in range(1, m):

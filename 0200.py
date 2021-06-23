185a61084813aa2660658ace88aea04c31617829
Yevhen Beskubskyi <yevhen.beskubskyi@gmail.com>
2021-06-23 16:04:45 +0300

    Update 0200.py

--- 0200.py
+++ 0200.py
@@ -1,25 +1,26 @@
-def bfs(i, j, grid):
-    if i < 0 or i >= len(grid):
-        return
-    if j < 0 or j >= len(grid[i]):
-        return
-    if grid[i][j] == '0':
-        return
-    grid[i][j] = '0'
-    bfs(i+1, j, grid)
-    bfs(i-1, j, grid)
-    bfs(i, j+1, grid)
-    bfs(i, j-1, grid)
+# Leetcode 0200 (Medium): Number of Islands
+# https://leetcode.com/problems/number-of-islands/
 
-def num_of_islands(grid):
-    '''
-    Leetcode 0200 (Medium): Number of Islands
-    https://leetcode.com/problems/number-of-islands/
-    '''
-    count = 0
-    for i in range(len(grid)):
-        for j in range(len(grid[i])):
-            if grid[i][j] == '1':
-                count += 1
-                bfs(i, j, grid)
-    return count
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+        def bfs(i, j):
+            if i < 0 or i >= len(grid):
+                return
+            if j < 0 or j >= len(grid[i]):
+                return
+            if grid[i][j] == '0':
+                return
+            grid[i][j] = '0'
+            
+            bfs(i+1, j)
+            bfs(i-1, j)
+            bfs(i, j+1)
+            bfs(i, j-1)
+            
+        count = 0
+        for i in range(len(grid)):
+            for j in range(len(grid[i])):
+                if grid[i][j] == '1':
+                    bfs(i, j)
+                    count += 1
+        return count

5b0120f6d92a0c868964bedcad2bc7004746a963
Yevhen Beskubskyi <yevhen.beskubskyi@gmail.com>
2021-06-23 16:05:43 +0300

    Update 0739.py

--- 0739.py
+++ 0739.py
@@ -1,15 +1,15 @@
-def daily_temperatures(temperatures):
-    '''
-    Leetcode 0739 (Medium): Daily Temperatures
-    https://leetcode.com/problems/daily-temperatures/
-    '''
-    stack = []
-    res = [0] * len(temperatures)
-    
-    for i, t in enumerate(temperatures):
-        while stack and stack[-1][0] < t:
-            tmp, idx = stack.pop()
-            res[idx] = i - idx
-        stack.append([t, i])
-    
-    return res
+# Leetcode 0739 (Medium): Daily Temperatures
+# https://leetcode.com/problems/daily-temperatures/
+
+class Solution:
+    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        stack = []
+        result = [0] * len(temperatures)
+        
+        for i, t in enumerate(temperatures):
+            while stack and stack[-1][0] < t:
+                temp, idx = stack.pop()
+                result[idx] = i - idx
+            stack.append([t, i])
+        
+        return result

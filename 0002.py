7b5c2feaf202fa6dd9f827efa21109f4cc9f1798
Yevhen Beskubskyi <yevhen.beskubskyi@gmail.com>
2021-06-23 10:47:48 +0300

    Update 0002.py

--- 0002.py
+++ 0002.py
@@ -1,27 +1,29 @@
+# Leetcode 0002 (Medium): Add Two Numbers
+# https://leetcode.com/problems/add-two-numbers/
+
+# Definition for singly-linked list.
 class ListNode:
-    def __init__(self, val):
+    def __init__(self, val=0, next=None):
         self.val = val
-        self.next = None
+        self.next = next
 
-def add_two_nodes(l1, l2):
-    '''
-    Leetcode 0002 (Medium): Add Two Numbers
-    https://leetcode.com/problems/add-two-numbers/
-    '''
-    memo = 0
-    dummy = ListNode(-1)
-    current = dummy
 
-    while l1 or l2:
-        val1 = l1.val if l1 else 0
-        val2 = l2.val if l2 else 0
-        current.next = ListNode((val1+val2+memo) % 10)
-        memo = (val1+val2+memo) // 10
-        if l1: l1 = l1.next
-        if l2: l2 = l2.next
-        current = current.next
+class Solution:
+    def addTwoNumbers(self, l1: ListNode, l2: ListNode) -> ListNode:
+        memo = 0
+        dummy = ListNode(-1)
+        current = dummy
+        
+        while l1 or l2:
+            val1 = l1.val if l1 else 0
+            val2 = l2.val if l2 else 0
+            current.next = ListNode((val1 + val2 + memo) % 10)
+            memo = (val1 + val2 + memo) // 10
+            if l1: l1 = l1.next
+            if l2: l2 = l2.next
+            current = current.next
 
-    if memo > 0:
-        current.next = ListNode(memo)
-    
-    return dummy.next
+        if memo > 0:
+            current.next = ListNode(memo)
+        
+        return dummy.next

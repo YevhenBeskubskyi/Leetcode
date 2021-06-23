17b11818b44dbaecf4b62b774a09c0d4922abc4f
Yevhen Beskubskyi <yevhen.beskubskyi@gmail.com>
2021-06-23 12:58:42 +0300

    Update 0062.py

--- 0062.py
+++ 0062.py
@@ -1,12 +1,10 @@
-def unique_paths(m, n):
-    '''
-    Leetcode 0062 (Medium): Unique Paths
-    https://leetcode.com/problems/unique-paths/
-    '''
-    grid = [[1] * n for _ in range(m)]
-    for i in range(1, m):
-        for j in range(1, n):
-            x = grid[i-1][j]
-            y = grid[i][j-1]
-            grid[i][j] = x + y
-    return grid[-1][-1]
+# Leetcode 0062 (Medium): Unique Paths
+# https://leetcode.com/problems/unique-paths/
+
+class Solution:
+    def uniquePaths(self, m: int, n: int) -> int:
+        grid = [[1] * n for _ in range(m)]
+        for i in range(1, m):
+            for j in range(1, n):
+                grid[i][j] = grid[i-1][j] + grid[i][j-1]
+        return grid[-1][-1]

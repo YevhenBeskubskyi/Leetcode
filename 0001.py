1b690b0362529e8eca97be901ea15aaa279bad68
Yevhen Beskubskyi <yevhen.beskubskyi@gmail.com>
2021-06-23 10:46:26 +0300

    Update 0001.py

--- 0001.py
+++ 0001.py
@@ -1,13 +1,11 @@
-def two_sum(nums, target):
-    '''
-    Leetcode 0001 (Easy): Two Sum
-    https://leetcode.com/problems/two-sum/
-    '''
-    diff = dict()
-    
-    for i, num in enumerate(nums):
-        if num in diff:
-            return [diff[num], i]
-        diff[target-num] = i
-    
-    return [-1, -1]
+# Leetcode 0001 (Easy): Two Sum
+# https://leetcode.com/problems/two-sum/
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        d = dict()
+        for i in range(len(nums)):
+            if nums[i] in d:
+                return i, d[nums[i]]
+            d[target-nums[i]] = i
+        return [-1, -1]

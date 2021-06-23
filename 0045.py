721fefab8e4538943a5cc227b9f9eef3d0e8c2a1
Yevhen Beskubskyi <yevhen.beskubskyi@gmail.com>
2021-06-23 12:56:08 +0300

    Update 0045.py

--- 0045.py
+++ 0045.py
@@ -1,10 +1,11 @@
-def jump(nums):
-    '''
-    Leetcode 45 (Medium): Jump Game II 
-    https://leetcode.com/problems/jump-game-ii/
-    '''
-    jumps = [i for i in range(len(nums))]
-    for i in range(len(jumps)):
-        for j in range(i+1, min(len(jumps), nums[i]+i+1)):
-            jumps[j] = min(jumps[j], jumps[i]+1)
-    return jumps[-1]
+# Leetcode 45 (Medium): Jump Game II 
+# https://leetcode.com/problems/jump-game-ii/
+
+class Solution:
+    def jump(self, nums: List[int]) -> int:
+        steps = [float('inf') for _ in range(len(nums))]
+        steps[0] = 0
+        for i in range(len(nums)):
+            for j in range(i+1, min(i+nums[i]+1, len(nums))):
+                steps[j] = min(steps[j], steps[i] + 1)
+        return steps[-1]

8e007f419599a88b6ef3f103432fdeff412f7683
Yevhen Beskubskyi <yevhen.beskubskyi@gmail.com>
2021-06-23 10:48:53 +0300

    Update 0003.py

--- 0003.py
+++ 0003.py
@@ -1,17 +1,19 @@
-def length_of_longest_substring(s):
-    '''
-    Leetcode 0003 (Medium): Longest Substring Without Repeating Character
-    https://leetcode.com/problems/longest-substring-without-repeating-characters/
-    '''
-    letters = set()
-    max_len = 0
+# Leetcode 0003 (Medium): Longest Substring Without Repeating Character
+# https://leetcode.com/problems/longest-substring-without-repeating-characters/
 
-    j = 0
-    for i in range(len(s)):
-        while s[i] in letters:
-            letters.remove(s[j])
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        letters = set()
+        maxlen = 0
+        
+        i, j = 0, 0
+        while j < len(s):
+            letter = s[j]
+            while letter in letters:
+                letters.remove(s[i])
+                i += 1
+            letters.add(letter)
+            maxlen = max(len(letters), maxlen)
             j += 1
-        letters.add(s[i])
-        max_len = max(max_len, len(letters))
-
-    reurn max(max_len, len(letters))
+            
+        return max(len(letters), maxlen)

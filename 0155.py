b66ee37778f4afa81ad3844a815184418f4d69b3
Yevhen Beskubskyi <yevhen.beskubskyi@gmail.com>
2021-06-29 16:59:30 +0300

    Create 0155.py

---
+++ 0155.py
@@ -0,0 +1,39 @@
+# Leetcode 0155 (Easy): Min Stack
+# https://leetcode.com/problems/min-stack/
+
+class MinStack:
+
+    def __init__(self):
+        """
+        initialize your data structure here.
+        """
+        self.stack = []
+        self.minStack = []
+        
+
+    def push(self, val: int) -> None:
+        self.stack.append(val)
+        minVal = min(val, self.minStack[-1] if self.minStack else val)
+        self.minStack.append(minVal)
+        
+
+    def pop(self) -> None:
+        self.stack.pop()
+        self.minStack.pop()
+            
+
+    def top(self) -> int:
+        return self.stack[-1]
+        
+
+    def getMin(self) -> int:
+        return self.minStack[-1] if self.minStack else 0
+        
+
+
+# Your MinStack object will be instantiated and called as such:
+# obj = MinStack()
+# obj.push(val)
+# obj.pop()
+# param_3 = obj.top()
+# param_4 = obj.getMin()

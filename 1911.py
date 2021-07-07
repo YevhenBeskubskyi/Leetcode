c8699559d28853f492235631ba1c1442f13423e0
Yevhen Beskubskyi <yevhen.beskubskyi@gmail.com>
2021-07-07 20:47:06 +0300

    Create 1911.py

---
+++ 1911.py
@@ -0,0 +1,21 @@
+# Leetcode 1911 (Medium): Maximum Alternating Subsequence Sum
+# https://leetcode.com/problems/maximum-alternating-subsequence-sum/
+
+class Solution:
+    def maxAlternatingSum(self, nums: List[int]) -> int:
+        tab = {}
+        
+        def dfs(i, even):
+            if i == len(nums):
+                return 0
+            if (i, even) in tab:
+                return tab[(i, even)]
+            
+            total = nums[i] if even else -nums[i]
+            x = total + dfs(i + 1, not even)
+            y = dfs(i + 1, even)
+            tab[(i, even)] = max(x, y)
+            
+            return tab[(i, even)]
+        
+        return dfs(0, True)

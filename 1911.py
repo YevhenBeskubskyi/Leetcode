694bdc449823187910f85d3181423ae3e31840ef
Yevhen Beskubskyi <yevhen.beskubskyi@gmail.com>
2021-07-07 20:57:57 +0300

    Update 1911.py

--- 1911.py
+++ 1911.py
@@ -3,19 +3,28 @@
 
 class Solution:
     def maxAlternatingSum(self, nums: List[int]) -> int:
-        tab = {}
+        evensum, oddsum = 0, 0
+        for i in range(len(nums) -1, -1, -1):
+            tmpeven = max(oddsum + nums[i], evensum)
+            tmpodd = max(evensum - nums[i], oddsum)
+            evensum, oddsum = tmpeven, tmpodd
+        return evensum
+
+# class Solution:
+#     def maxAlternatingSum(self, nums: List[int]) -> int:
+#         tab = {}
         
-        def dfs(i, even):
-            if i == len(nums):
-                return 0
-            if (i, even) in tab:
-                return tab[(i, even)]
+#         def dfs(i, even):
+#             if i == len(nums):
+#                 return 0
+#             if (i, even) in tab:
+#                 return tab[(i, even)]
             
-            total = nums[i] if even else -nums[i]
-            x = total + dfs(i + 1, not even)
-            y = dfs(i + 1, even)
-            tab[(i, even)] = max(x, y)
+#             total = nums[i] if even else -nums[i]
+#             x = total + dfs(i + 1, not even)
+#             y = dfs(i + 1, even)
+#             tab[(i, even)] = max(x, y)
             
-            return tab[(i, even)]
+#             return tab[(i, even)]
         
-        return dfs(0, True)
+#         return dfs(0, True)

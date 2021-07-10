b7201f9d848ec8ba2c8a5d78086be376d0bf39d0
Yevhen Beskubskyi <yevhen.beskubskyi@gmail.com>
2021-07-10 23:34:28 +0300

    Create 0017.py

---
+++ 0017.py
@@ -0,0 +1,30 @@
+class Solution:
+    def letterCombinations(self, digits: str) -> List[str]:
+        if not digits: return []
+        
+        chars = {}
+        chars['2'] = 'abc'
+        chars['3'] = 'def'
+        chars['4'] = 'ghi'
+        chars['5'] = 'jkl'
+        chars['6'] = 'mno'
+        chars['7'] = 'pqrs'
+        chars['8'] = 'tuv'
+        chars['9'] = 'wxyz'
+        
+        def addChar(char, combinations):
+            for i in range(len(combinations)):
+                combinations[i] += char
+            return combinations
+        
+        def addDigit(digit, combinations):
+            digitCombinations = []
+            digitChars = chars[digit]
+            for char in digitChars:
+                digitCombinations += addChar(char, combinations[:])
+            return digitCombinations
+        
+        res = ['']
+        for digit in digits:
+            res = addDigit(digit, res)
+        return sorted(res)
